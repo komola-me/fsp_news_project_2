45a214837a21942a1bfd674a917f48176b8e0c32
komola-me <komolaziya28@gmail.com>
2024-02-19 11:55:53 +0500

    add: comment model and form

--- news_app/admin.py
+++ news_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import News, Category, Contact
+from .models import News, Category, Contact, Comment
 
 # Register your models here.
 # admin.site.register(News)
@@ -27,3 +27,20 @@ admin.site.register(Category, CategoryAdmin)
 
 
 admin.site.register(Contact)
+
+# 2nd option
+# @admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'body', 'created_time', 'active']
+    list_filter = ['active', 'created_time']
+    search_fields = ['user', 'body']
+    actions = ['disable_comments', 'activate_comments']
+
+    def disable_comments(self, request, queryset):
+        queryset.update(active=False)
+
+    def activate_comments(self, request, queryset):
+        queryset.update(active=True)
+
+# 1st option
+admin.site.register(Comment, CommentAdmin)
